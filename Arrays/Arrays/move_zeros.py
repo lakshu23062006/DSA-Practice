90b40070b0ee96544688b689d15a979b42beda13
Lakshmi G <lakshu23062006.com@gmail.com>
2026-07-31 21:50:24 +0530

    Add function to move zeroes in an array
    Implemented a solution to move all zeroes in an array to the end while maintaining the order of non-zero elements using a two-pointer approach.

--- Arrays/Arrays/move_zeros.py
+++ Arrays/Arrays/move_zeros.py
@@ -1,1 +1,18 @@
-print("hello github")
+"""
+Problem: Move Zeroes
+Platform: LeetCode
+Approach: Two Pointers
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+nums = list(map(int, input("Enter array elements: ").split()))
+
+j = 0
+
+for i in range(len(nums)):
+    if nums[i] != 0:
+        nums[j], nums[i] = nums[i], nums[j]
+        j += 1
+
+print("Output:", nums)
